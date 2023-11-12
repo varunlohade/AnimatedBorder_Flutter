66d0c600f94c79a043ca47eae345be5cd4b0c120
varunlohade <varunlohade@gmail.com>
2023-11-12 14:45:06 +0530

    final

---
+++ lib/main.dart
@@ -0,0 +1,212 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 8),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 120,
+              width: 312,
+              decoration: BoxDecoration(
+                  color: Colors.black, borderRadius: BorderRadius.circular(12)),
+            ),
+            AnimatedBuilder(
+                animation: _controller,
+                builder: (context, child) {
+                  double angle = _controller.value * 2 * 3.14;
+                  double x = 0.7 +
+                      0.4 *
+                          sin(angle); // Adjust these values for the desired effect
+                  double y = 0.7 +
+                      0.9 *
+                          cos(angle); // Adjust these values for the desired effect
+                  // 2 * pi, full rotation
+
+                  return UnicornOutlineButton(
+                    strokeWidth: 1,
+                    radius: 16,
+                    gradient: LinearGradient(
+                      colors: const [
+                        Color(0xffffffff),
+                        Color(0xff99ffffff),
+                        Color(0xff00000000)
+                      ],
+                      begin: Alignment(x, y),
+                      end: Alignment(1.0 - x, 1.0 - y),
+                    ),
+                    child: Container(
+                      width: 140,
+                      height: 50,
+                      child: const Center(
+                        child: Text('Enter',
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white)),
+                      ),
+                    ),
+                    onPressed: () {},
+                  );
+                }),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class UnicornOutlineButton extends StatelessWidget {
+  final _GradientPainter _painter;
+  final Widget _child;
+  final VoidCallback _callback;
+  final double _radius;
+
+  UnicornOutlineButton({
+    required double strokeWidth,
+    required double radius,
+    required Gradient gradient,
+    required Widget child,
+    required VoidCallback onPressed,
+  })  : this._painter = _GradientPainter(
+            strokeWidth: strokeWidth, radius: radius, gradient: gradient),
+        this._child = child,
+        this._callback = onPressed,
+        this._radius = radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _painter,
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: _callback,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(_radius),
+          onTap: _callback,
+          child: Container(
+            constraints: BoxConstraints(minWidth: 88, minHeight: 48),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _child,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _GradientPainter extends CustomPainter {
+  final Paint _paint = Paint();
+  final double radius;
+  final double strokeWidth;
+  final Gradient gradient;
+
+  _GradientPainter(
+      {required double strokeWidth,
+      required double radius,
+      required Gradient gradient})
+      : this.strokeWidth = strokeWidth,
+        this.radius = radius,
+        this.gradient = gradient;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // create outer rectangle equals size
+    Rect outerRect = Offset.zero & size;
+    var outerRRect =
+        RRect.fromRectAndRadius(outerRect, Radius.circular(radius));
+
+    // create inner rectangle smaller by strokeWidth
+    Rect innerRect = Rect.fromLTWH(strokeWidth, strokeWidth,
+        size.width - strokeWidth * 2, size.height - strokeWidth * 2);
+    var innerRRect = RRect.fromRectAndRadius(
+        innerRect, Radius.circular(radius - strokeWidth));
+
+    // apply gradient shader
+    _paint.shader = gradient.createShader(outerRect);
+
+    // create difference between outer and inner paths and draw it
+    Path path1 = Path()..addRRect(outerRRect);
+    Path path2 = Path()..addRRect(innerRRect);
+    var path = Path.combine(PathOperation.difference, path1, path2);
+    canvas.drawPath(path, _paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => oldDelegate != this;
+}
